f0e2a288f76d54f06532d914415d6104f9e0efda
dev <dev@dev.com>
2022-08-09 22:26:18 +0530

    commit..

--- 4 JAVA/ws-eclipse/d1-java-master/src/demo/basics/StaticVsInstanceVariablesEx.java
+++ 4 JAVA/ws-eclipse/d1-java-master/src/demo/basics/StaticVsInstanceVariablesEx.java
@@ -1,19 +1,23 @@
 package demo.basics;
 
 public class StaticVsInstanceVariablesEx {
-public static void main(String[] args) {
-	// directly accessed with class name
-	String companyName  = Employee.companyName;
-	System.err.println(companyName);
-	Employee employee1 = new Employee();
-	employee1.employeeName = "Sam";
-	Employee employee2 = new Employee();
-	employee2.employeeName = "Peter";
-	String employeeName1 = employee1.employeeName;
-	String employeeName2 = employee2.employeeName;
-	System.err.println(employeeName1);
-	System.err.println(employeeName2);
-}
+	public static void main(String[] args) {
+		// directly accessed with class name
+		String companyName = Employee.companyName;
+		System.err.println(companyName);
+		// new keyword creates instance
+		Employee employee1 = new Employee();
+		employee1.employeeName = "Sam";
+		employee1.clientName = "Paypal";
+		Employee employee2 = new Employee();
+		employee2.employeeName = "Peter";
+		String employeeName1 = employee1.employeeName;
+		employee2.clientName = "Amazon";
+		String client1 = employee1.clientName;
+		String client2 = employee2.clientName;
+		String employeeName2 = employee2.employeeName;
+		System.err.println(employeeName1 + " works for " + client1);		
+		System.err.println(employeeName2 + " works for " + client2);	}
 }
 
 class Employee {
@@ -21,5 +25,5 @@ class Employee {
 	public static String companyName = "The Amazing Company";
 //	intance variable / belongs to instance
 	public String employeeName;
-	
+	public String clientName;
 }
