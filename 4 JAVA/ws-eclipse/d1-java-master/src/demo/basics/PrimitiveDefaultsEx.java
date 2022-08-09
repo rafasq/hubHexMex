1f5c9014e278619685b7a192583be14a48350dd1
dev <dev@dev.com>
2022-08-09 20:09:00 +0530

    commit..

---
+++ 4 JAVA/ws-eclipse/d1-java-master/src/demo/basics/PrimitiveDefaultsEx.java
@@ -0,0 +1,25 @@
+package demo.basics;
+
+public class PrimitiveDefaultsEx {
+//	initialized by default
+	byte bvar;
+	short shortvar;
+	int intvar;
+	long longvar;
+	float floatvar;
+	double doublevar;
+	char charvar;
+	boolean booleanvar;
+	
+	public static void main(String[] args) {
+		PrimitiveDefaultsEx object = new PrimitiveDefaultsEx();
+		System.err.println(object.bvar);
+		System.err.println(object.shortvar);
+		System.err.println(object.intvar);
+		System.err.println(object.longvar);
+		System.err.println(object.floatvar);
+		System.err.println(object.doublevar);
+		System.err.println(object.charvar);
+		System.err.println(object.booleanvar);
+	}
+}
