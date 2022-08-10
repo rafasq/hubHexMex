228b6536abc34c26ef5f8352790d7dc5a74df901
dev <dev@dev.com>
2022-08-10 19:24:45 +0530

    commit..

---
+++ 4 JAVA/ws-eclipse/d1-java-master/src/demo/basics/WrongMain.java
@@ -0,0 +1,18 @@
+package demo.basics;
+
+public class WrongMain {
+	public static void main(String[] args) {
+		System.err.println("oh yes....");
+	}
+//	public static void main(String[] anything) {
+//		System.err.println("oh yes....");
+//	}
+//	static public void main(String[] anything) {
+//		System.err.println("oh yes....");
+//	}
+//	static public int main(String[] anything) {
+//		System.err.println("oh no....");
+//		return 0;
+//	}
+	
+}
--- 4 JAVA/ws-eclipse/d1-java-master/src/demo/oops/BonusCalculator.java
+++ 4 JAVA/ws-eclipse/d1-java-master/src/demo/oops/BonusCalculator.java
@@ -2,17 +2,19 @@ package demo.oops;
 
 public class BonusCalculator {
 	static int salary = 50;
+
 // param bonus / returns total
 	float calculateTotalSalary(float bonus) {
 		float total;
-		return salary + bonus;
+		total  = salary + bonus;
+		return total;
 	}
 
 	public static void main(String[] args) {
 //		create object
 		BonusCalculator bonusCalculatorObject = new BonusCalculator();
 		// calling function on object
-		float bonus = bonusCalculatorObject.calculateTotalSalary(5);
-		System.err.println(bonus);
+		float total = bonusCalculatorObject.calculateTotalSalary(5);
+		System.err.println(total);
 	}
 }
