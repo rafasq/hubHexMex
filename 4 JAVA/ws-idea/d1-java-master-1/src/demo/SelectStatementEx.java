fd5ba5843217cf459b3d178d883d09e4b15d0cc8
dev <dev@dev.com>
2022-08-16 19:40:45 +0530

    commit..

---
+++ 4 JAVA/ws-idea/d1-java-master-1/src/demo/SelectStatementEx.java
@@ -0,0 +1,51 @@
+package demo;
+
+import java.sql.*;
+
+public class SelectStatementEx {
+    public static void main(String[] args) {
+        //add mysql-connector-java-8.0.27 to project
+        Connection connection = null;
+        try {
+            //load the driver // handle ClassNotFoundException.
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded.");
+//            connect
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/SAKILA","root","mysql");
+//          query
+            System.out.println("connected");
+            String selectQuery = "SELECT ACTOR_ID, FIRST_NAME, LAST_NAME FROM ACTOR WHERE ACTOR_ID < 6";
+            // statement
+            System.out.println("Query created");
+            Statement selectStatement = connection.createStatement();
+            System.out.println("query executed");
+            // execute query on statement / get ResultSet
+            ResultSet resultSet = selectStatement.executeQuery(selectQuery);
+            System.out.println("iterate resultset");
+            // iterate
+            while(resultSet.next()) {
+//              int actorId =   resultSet.getInt("actor_id");
+//              String firstName = resultSet.getString("first_name");
+//                String lastName = resultSet.getString("last_name");
+
+                int actorId =   resultSet.getInt(1);
+                String firstName = resultSet.getString(2);
+                String lastName = resultSet.getString(3);
+
+                System.out.println(actorId + "\t" + firstName + "\t" + lastName );
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver not found");
+        } catch (SQLException e) {
+            System.out.println("Connect failed..");
+        }
+        finally {
+            System.out.println("close connection");
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println("failed closing con");
+            }
+        }
+    }
+}
