3e9b623c750da1d98c2ec1d39807c30c2f363ae5
devs <dev@dev.com>
2022-08-30 20:55:53 +0530

    commit..

--- 6 JUNIT/ld1-junit/src/test/java/com/demo/MessageUtilTest.java
+++ 6 JUNIT/ld1-junit/src/test/java/com/demo/MessageUtilTest.java
@@ -2,15 +2,40 @@ package com.demo;
 
 //import  org.junit.Assert;
 import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+
+import org.junit.*;
 
 public class MessageUtilTest {
     String message = "Hello World!!";
     MessageUtil messageUtil = new MessageUtil(message);
+//    executed once in the beginning
+    @BeforeClass
+    public static void setUp(){
+        System.out.println("in before class .. logging into a database!");
+    }
+    @Before
+    public void before(){
+        System.out.println("before each test case");
+    }
 
+//    Test Case
     @Test
     public void testPrintMessage() {
 //        Assert.assertEquals("Hello World!!", messageUtil.printMessage());
         assertEquals("Hello World!!", messageUtil.printMessage());
+    }
+    @Test
+    public void testPrintMessage2() {
+//        Assert.assertEquals("Hello World!!", messageUtil.printMessage());
+        assertEquals("Hello World!!", messageUtil.printMessage());
+    }
+
+    @After
+    public void after(){
+        System.out.println("after each test case");
+    }
+    @AfterClass
+    public static void tearDown(){
+        System.out.println("in after class .. logging out of a database!");
     }
 }
