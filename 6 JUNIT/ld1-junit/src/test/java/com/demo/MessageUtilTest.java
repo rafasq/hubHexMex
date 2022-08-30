a01db869225d2974de92ea15873dddc7d996e0b1
devs <dev@dev.com>
2022-08-30 18:54:42 +0530

    commit..

--- 6 JUNIT/ld1-junit/src/test/java/com/demo/MessageUtilTest.java
+++ 6 JUNIT/ld1-junit/src/test/java/com/demo/MessageUtilTest.java
@@ -1,15 +1,16 @@
 package com.demo;
 
+//import  org.junit.Assert;
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
-public class TestJUnit {
-    String message = "Hello World..";
+public class MessageUtilTest {
+    String message = "Hello World!!";
     MessageUtil messageUtil = new MessageUtil(message);
 
     @Test
-    public void printMessage() {
+    public void testPrintMessage() {
+//        Assert.assertEquals("Hello World!!", messageUtil.printMessage());
         assertEquals("Hello World!!", messageUtil.printMessage());
     }
 }
